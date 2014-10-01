1bf379555244079b6d31936bdf1bcbd020e7ea8b
Jens Norrgrann <jens.norrgrann@bukowskis.com>
2014-10-01 12:38:44 +0200

    Don't crash on invalid json

--- server/lib/message_handler.rb
+++ server/lib/message_handler.rb
@@ -3,5 +3,7 @@ class MessageHandler < EM::Connection
   def receive_data(data)
     data = JSON.parse(data)
     Backend.run(data)
+  rescue JSON::ParserError
+    puts "That ain't no valid JSON"
   end
 end
