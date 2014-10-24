93faa9c01cb4624612a19d79c2040835045ecc89
Jens Norrgrann <jens.norrgrann@bukowskis.com>
2014-10-24 10:17:49 +0200

    Spelling is hard

--- lib/middleware/influx_middleware.rb
+++ lib/middleware/influx_middleware.rb
@@ -4,7 +4,7 @@ require 'active_support'
 class InfluxMiddleware
   def initialize(app)
     @app = app
-    @influxdb = influxdb = InfluxDB::Client.new ENV['INFLUXDB_DATABASE'], host: ENV['INFLUXDB_HOST'], username: ENV['INFLUXDB_USER'], password: ENV['INFLUXDB_PASSSWORD'], async: true
+    @influxdb = influxdb = InfluxDB::Client.new ENV['INFLUXDB_DATABASE'], host: ENV['INFLUXDB_HOST'], username: ENV['INFLUXDB_USER'], password: ENV['INFLUXDB_PASSWORD'], async: true
   end
 
   def call(env)
