25555a0aca7b903f755d7f672db55abe6e6e5293
mmzoo <b@noteyard.com>
2014-11-28 13:17:42 +0100

    Transpec https://github.com/yujinakayama/transpec

--- spec/client/lib/meter/configuration_spec.rb
+++ spec/client/lib/meter/configuration_spec.rb
@@ -3,7 +3,7 @@ require 'meter/configuration'
 
 describe Meter::Configuration do
 
-  let(:logger) { mock(:logger) }
+  let(:logger) { double(:logger) }
   let(:config)  { Meter.config }
 
   before do
@@ -14,14 +14,14 @@ describe Meter::Configuration do
 
     describe '#logger' do
       it 'is an STDOUT logger' do
-        Logger.should_receive(:new).with(STDOUT).and_return logger
-        config.logger.should be logger
+        expect(Logger).to receive(:new).with(STDOUT).and_return logger
+        expect(config.logger).to be logger
       end
 
       context 'with Rails' do
         before do
           ensure_module :Rails
-          Rails.stub!(:logger).and_return(logger)
+          allow(Rails).to receive(:logger).and_return(logger)
         end
 
         after do
@@ -29,30 +29,30 @@ describe Meter::Configuration do
         end
 
         it 'is the Rails logger' do
-          config.logger.should be Rails.logger
+          expect(config.logger).to be Rails.logger
         end
       end
     end
 
     describe '#primary_backend and #secondary_backend' do
       it 'is a Backend' do
-        config.primary_backend.should be_instance_of Meter::Backend
-        config.secondary_backend.should be_instance_of Meter::Backend
+        expect(config.primary_backend).to be_instance_of Meter::Backend
+        expect(config.secondary_backend).to be_instance_of Meter::Backend
       end
 
       it 'has the default host' do
-        config.primary_backend.host.should == '127.0.0.1'
-        config.secondary_backend.host.should == '127.0.0.1'
+        expect(config.primary_backend.host).to eq('127.0.0.1')
+        expect(config.secondary_backend.host).to eq('127.0.0.1')
       end
 
       it 'has the default namespace' do
-        config.primary_backend.namespace.should == nil
-        config.secondary_backend.namespace.should == nil
+        expect(config.primary_backend.namespace).to eq(nil)
+        expect(config.secondary_backend.namespace).to eq(nil)
       end
 
       it 'has the default port' do
-        config.primary_backend.port.should == 8125
-        config.secondary_backend.port.should == 8126
+        expect(config.primary_backend.port).to eq(8125)
+        expect(config.secondary_backend.port).to eq(8126)
       end
     end
 
--- spec/client/lib/meter_spec.rb
+++ spec/client/lib/meter_spec.rb
@@ -10,26 +10,26 @@ describe Meter do
   describe '.increment' do
     context "when given an id as option"  do
       it "proxies to the counter with the id" do
-        counter.should_receive(:increment).with("my.key.123", {})
+        expect(counter).to receive(:increment).with("my.key.123", {})
         meter.increment 'my.key', :id => 123
       end
 
       it "proxies to the primary and secondary without the id" do
-        primary.should_receive(:increment).with("my.key", {})
-        secondary.should_receive(:increment).with("my.key", {})
+        expect(primary).to receive(:increment).with("my.key", {})
+        expect(secondary).to receive(:increment).with("my.key", {})
         meter.increment 'my.key', :id => 123
       end
     end
 
     context "when id is NOT given as an option"  do
       it "doesn't proxy to the counter at all" do
-        counter.should_not_receive(:increment)
+        expect(counter).not_to receive(:increment)
         meter.increment 'my.key'
       end
 
       it "proxies to the primary and secondary" do
-        primary.should_receive(:increment).with("my.key", {})
-        secondary.should_receive(:increment).with("my.key", {})
+        expect(primary).to receive(:increment).with("my.key", {})
+        expect(secondary).to receive(:increment).with("my.key", {})
         meter.increment 'my.key'
       end
     end
