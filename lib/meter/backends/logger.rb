d5fb9f59ee65236d74852659664c60dcc9173f9a
Jens Norrgrann <jens.norrgrann@bukowskis.com>
2019-05-03 12:08:14 +0200

    Add backend for plain logger
    Instead of using the JsonLog backend with its application.json.log this uses the configured logger to emit logs instead.

---
+++ lib/meter/backends/logger.rb
@@ -0,0 +1,21 @@
+module Meter
+  module Backends
+    class Logger < ::Meter::Backends::Base
+      def self.supported_metrics
+        [:log]
+      end
+
+      def convert_to_backend_format(metric)
+        {
+          statname:    metric.name,
+          metric_type: metric.type,
+          metric_value: metric.value
+        }.merge(metric.data).merge(metric.tags)
+      end
+
+      def output_data(data)
+        ::Meter.config.logger.info data
+      end
+    end
+  end
+end
